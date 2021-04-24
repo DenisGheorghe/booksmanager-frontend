a44ce4734d53410e1dda7191e92e28febc239c67
Denis <gheorghe.n.denismarian18@student.rau.ro>
2021-04-24 22:46:57 +0300

    chore: add basic vue scaffolding

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+  <h1>Aplicatia lui Denis</h1>
+  <pre>{{userData}}</pre>
+
+  <ul>
+    <li v-for="username in userData.users" :key="username"> {{username}}</li>
+  </ul>
+</div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'app',
+  methods: {
+
+  },
+  data () {
+    return {
+      userData: {}
+    }
+  },
+   mounted () {
+    axios
+      .get('http://localhost:4000/users')
+      .then(response => (this.userData = response.data))
+  }
+}
+</script>
