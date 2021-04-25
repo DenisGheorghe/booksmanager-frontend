af00aaccd80a31ede3ed79e79e16cab4d12b5825
Denis <gheorghe.n.denismarian18@student.rau.ro>
2021-04-25 13:59:59 +0300

    feature: home and contact

---
+++ src/pages/home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>This is the homepage</h1>
+    </div>
+</template>
+<script>
+export default {
+
+    data(){
+        return{}
+    }
+}
+</script>
+<style scoped>
+
+</style>
